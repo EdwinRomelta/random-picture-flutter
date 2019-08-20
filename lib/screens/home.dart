eac80d6670c3443af63d1f7869f9f48848216bc9
Edwin Romelta <ImNotProZ@gmail.com>
2019-08-20 23:31:26 +0700

    Implement Home Screen and fix slash screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home"),
+        actions: null,
+      ),
+    );
+  }
+}
